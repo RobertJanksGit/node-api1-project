f9496b7c9d2f9f4cc3e9278434dea8379e64b19c
Robert Jank <robert.jank@yahoo.com>
2024-10-03 15:13:50 -0500

    added getById

--- index.js
+++ index.js
@@ -1,5 +1,12 @@
-const server = require('./api/server');
+const server = require("./api/server");
 
 const port = 9000;
 
 // START YOUR SERVER HERE
+server.get("/", (req, res) => {
+  res.send("Hello from Express!");
+});
+
+server.listen(port, () => {
+  console.log(`server is listening on port ${port}`);
+});
